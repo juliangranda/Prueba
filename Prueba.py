c115839c6f488bae99a0c278a52ebb0c745780ea
juliangranda <juliangranda@hotmail.com>
2025-11-12 18:46:05 -0500

    Add a change with a conditional

--- Prueba.py
+++ Prueba.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 
+'''
 ventana = tk.Tk()
 ventana.title("Mi Prueba")
 tk.Label(ventana,text="hola mundo").pack()
-ventana.mainloop()
+ventana.mainloop()'''
+
+caballo = 10
+liebre = 5
+
+if caballo > liebre:
+    print("El caballo es rapido que la liebre")
